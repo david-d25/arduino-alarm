d32737d4b1059af4cc45084f9016a585bd34f82c
David <david-d25@yandex.ru>
2018-08-29 23:18:31 +0300

    Revolution of code

--- alarm.ino
+++ alarm.ino
@@ -1,9 +1,10 @@
+#include <LiquidCrystal_I2C.h>
+
 #include <Adafruit_Sensor.h>
 
 #include <DHT.h>
 #include <DHT_U.h>  
 
-#include <LiquidCrystal.h>
 #include <iarduino_RTC.h>
 
 byte degreeSymbol[8] = {
@@ -25,74 +26,117 @@ byte degreeSymbol[8] = {
 #define TIME_PIN_DAT 4
 #define TIME_PIN_RST 5
 
-#define LCD_BACKLIGHT 6
+#define PIN_LCD_BACKLIGHT 6
 
-#define LCD_PIN_RS 7
-#define LCD_PIN_E 8
-#define PIN_ALARMLED 9
-#define LCD_PIN_DB4 10
-#define LCD_PIN_DB5 11
-#define LCD_PIN_DB6 12
-#define LCD_PIN_DB7 13
+#define NONE_A 7
+#define NONE_B 8
+#define PIN_LED_INDICATOR 9
 
-#define PIN_DHT A0
-#define PIN_BTN A1
-#define PIN_LIGHT_DETECTOR A2
-#define PIN_WARM_LIGHT A3
-#define PIN_COLD_LIGHT A4
-#define PIN_DEBUG_MODE A5
+#define PIN_WARM_LIGHT 10
+#define PIN_COLD_LIGHT 11
+
+#define PIN_BTN A0
+#define PIN_LIGHT_DETECTOR A1
+#define PIN_DHT A2
+#define PIN_SWITCH A3
+#define PIN_LCD_SDA A4
+#define PIN_LCD_SCL A5
 
 boolean isAlarmActive = false;
 boolean isLightActive = true;
 
 iarduino_RTC time(RTC_DS1302, TIME_PIN_RST, TIME_PIN_CLK, TIME_PIN_DAT);
-LiquidCrystal lcd(LCD_PIN_RS, LCD_PIN_E, LCD_PIN_DB4, LCD_PIN_DB5, LCD_PIN_DB6, LCD_PIN_DB7);
+LiquidCrystal_I2C lcd(0x3f, LCD_WIDTH, LCD_HEIGHT);
 DHT dht(PIN_DHT, DHT11);
 
 int temperature = 0;
 int humidity = 0;
 
+int brightness = 0;
+int brightnessTarget = (int)(1024*0.24);
+
 void setup() {
-  pinMode(PIN_ALARMLED, OUTPUT);
+  pinMode(13, OUTPUT); // Onboard LED
+  digitalWrite(13, LOW);
+  
+  pinMode(PIN_LED_INDICATOR, OUTPUT);
   
   pinMode(PIN_BTN_INTERRUPT, INPUT_PULLUP);
+
+  pinMode(PIN_COLD_LIGHT, OUTPUT);
+  pinMode(PIN_WARM_LIGHT, OUTPUT);
+
+  pinMode(PIN_LIGHT_DETECTOR, INPUT_PULLUP);
+  pinMode(PIN_SWITCH, INPUT_PULLUP);
   
   attachInterrupt(0, onButtonPress, RISING);
+
   
-  lcd.begin(LCD_WIDTH, LCD_HEIGHT);
+  lcd.init();
+  lcd.backlight();
+
+  if (Serial)
+    Serial.begin(9600);
+  
   lcd.createChar(1, degreeSymbol);
-  time.begin();
+//  time.begin();
   dht.begin();
   updateHT();
 }
 
 void loop() {
-  if (millis()%5000==0)
-  {
+  if (getLightIntensity() < brightnessTarget) {
+    brightness += 3;
+    if (brightness > 1023) brightness = 1023;
+  }
+  if (getLightIntensity() > brightnessTarget) {
+    brightness -= 4;
+    if (brightness < 0) brightness = 0;
+  }
+
+  if (Serial && Serial.available() > 0) {
+    lcd.clear();
+    lcd.print(Serial.readString());
+  }
+  analogWrite(PIN_LCD_BACKLIGHT, max(255 - getLightIntensity()/4, brightnessFilter(brightness)));
+  
+  if (millis()%10 == 0)
     updateHT();
-  }
-  if (millis()%1000==0)
-  {
+  
+  if (digitalRead(PIN_SWITCH) == HIGH) {
     displayDateTime();
-    delay(1);
-  }
-  if (digitalRead(PIN_BTN) == 1)
-  {
-    turnLight(!isLightActive);
+    analogWrite(PIN_WARM_LIGHT, brightnessFilter(brightness));
+    analogWrite(PIN_COLD_LIGHT, brightnessFilter(brightness));
+    Serial.println(String(brightness) + " - " + String(brightnessFilter(brightness)) + " - " + String(getLightIntensity()));
+  } else {
+    analogWrite(PIN_COLD_LIGHT, 3);
+    analogWrite(PIN_WARM_LIGHT, 3);
   }
 }
 
 void onButtonPress() {
- // turnLight(!isLightActive);
+  
+}
+
+int getLightIntensity() {
+  return 1024 - analogRead(PIN_LIGHT_DETECTOR);
+}
+
+int brightnessFilter(int v) {
+  return map((tanh(v/1024.0*2*PI - PI) + 1)/2.0*1024, 0, 1024, 0, 256);
 }
 
 void displayDateTime()
 {
   lcd.home();
-  lcd.print(time.gettime("d/m/Y "));
-  lcd.print(time.gettime("D"));
+  //lcd.print(time.gettime("d/m/Y "));
+  //lcd.print(time.gettime("D"));
+  lcd.print("Light: " + String(map(getLightIntensity(), 0, 1024, 0, 100)) + "%");
   lcd.setCursor(0, 1);
-  lcd.print(time.gettime("h:i A"));
+  //lcd.print(time.gettime("h:i A"));
+  lcd.setCursor(6, 1);
+  lcd.print(humidity);
+  lcd.print("%");
   lcd.setCursor(11, 1);
   lcd.print(temperature);
   lcd.print("\1C  ");
@@ -102,24 +146,4 @@ void updateHT()
 {
   humidity = dht.readHumidity();
   temperature = dht.readTemperature();
-}
-
-void turnLight(boolean value)
-{
-  isLightActive = value;
-  if (value) {
-    analogWrite(PIN_LIGHT, 128);
-  } else {
-    analogWrite(PIN_LIGHT, 0);
-  }
-}
-
-void turnAlarm(boolean active)
-{
-  isAlarmActive = active;
-  if (isAlarmActive) {
-    digitalWrite(PIN_ALARMLED, 1);
-  } else {
-    digitalWrite(PIN_ALARMLED, 0);
-  }
 }
